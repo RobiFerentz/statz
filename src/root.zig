9f3246f8995c4600b510647f6e97f273ac95eda1
Robert Ferentz <robiferentz@gmail.com>
2025-10-16 22:49:21 +0300

    Initial commit

--- src/root.zig
+++ src/root.zig
@@ -1,6 +1,11 @@
 //! By convention, root.zig is the root source file when making a library. If
 //! you are making an executable, the convention is to delete this file and
 //! start with main.zig instead.
+pub const mean = @import("mean.zig").mean;
+pub const median = @import("median.zig").median;
+pub const mode = @import("mode.zig").mode;
+pub const std_dev = @import("std_dev.zig").std_dev;
+pub const sum_consecutive_numbers = @import("sum_consec.zig").sum_consecutive_numbers;
 
 test "mean" {
     _ = @import("mean.zig");
